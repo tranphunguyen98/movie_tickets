6dfd25fc523dce37c35abff4416b85efe2ac8abb
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2020-06-02 16:36:25 +0700

    feat (Home): add Widget recommend seats

--- lib/presentation/screen/home/sc_home.dart
+++ lib/presentation/screen/home/sc_home.dart
@@ -1,6 +1,6 @@
-import 'package:find_seat/presentation/common_widgets/widget_spacer.dart';
 import 'package:flutter/material.dart';
 
+import '../../common_widgets/barrel_common_widgets.dart';
 import 'barrel_home.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,6 +14,7 @@ class HomeScreen extends StatelessWidget {
             WidgetHomeBanner(),
             WidgetSpacer(height: 30),
             WidgetHomeCategories(),
+            WidgetSpacer(height: 30),
             WidgetRecommendedSeats(),
             WidgetNearbyTheatres(),
             WidgetHomeEvents(),
