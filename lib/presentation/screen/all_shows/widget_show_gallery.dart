680ece6e53cb7186cef1be312ec9eccc767f434b
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2020-06-03 19:53:43 +0700

    feat (AllShow): add list shows

--- lib/presentation/screen/all_shows/widget_show_gallery.dart
+++ lib/presentation/screen/all_shows/widget_show_gallery.dart
@@ -3,6 +3,8 @@ import 'package:find_seat/utils/my_const/my_const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'barrel_all_shows.dart';
+
 class WidgetShowGallery extends StatefulWidget {
   @override
   _WidgetShowGalleryState createState() => _WidgetShowGalleryState();
@@ -29,6 +31,21 @@ class _WidgetShowGalleryState extends State<WidgetShowGallery>
     super.initState();
   }
 
+  List<ItemShowVM> items = [
+    ItemShowVM(
+        "images/movie/fdf8fabbc12db9cddc60574f691d26c6@2x.png", "Bigil", 84),
+    ItemShowVM(
+        "images/movie/d4baeb81488e83cb54f041917ff62f59@2x.png", "Kaithi", 98),
+    ItemShowVM(
+        "images/movie/202693a21503970eac9090537062d5d3.png", "Gabbar", 84),
+    ItemShowVM("images/movie/81c106f1df72317e034fafa6c4975e9f.png",
+        "Pizza - part 2", 98),
+    ItemShowVM(
+        "images/movie/61d30e82f43b1cab9f49e576ae457086@2x.png", "Asuran", 94),
+    ItemShowVM(
+        "images/movie/3890dc6228535cbf5eebd947738d4e52.png", "Sarkar", 87)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,9 +55,9 @@ class _WidgetShowGalleryState extends State<WidgetShowGallery>
           child: TabBarView(
             controller: _controller,
             children: <Widget>[
-              Container(child: Text('1')),
-              Container(child: Text('2')),
-              Container(child: Text('3')),
+              WidgetListShow(items),
+              WidgetListShow(items.sublist(1, 3).toList()),
+              WidgetListShow(items.sublist(3, 5).toList()),
             ],
           ),
         )
@@ -71,7 +88,7 @@ class _WidgetShowGalleryState extends State<WidgetShowGallery>
         unselectedLabelStyle: FONT_CONST.REGULAR_GRAY1_12,
         indicatorSize: TabBarIndicatorSize.label,
         indicator: RoundedRectIndicator(
-            color: COLOR_CONST.DEFAULT, radius: 2, padding: 20, weight: 3.0),
+            color: COLOR_CONST.DEFAULT, radius: 2, padding: 22, weight: 3.0),
       ),
     );
   }
