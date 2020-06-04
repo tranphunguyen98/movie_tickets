c58377f871d8f471b32b5b29d2aed88ab796de2b
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2020-06-04 09:28:06 +0700

    feat: add navigation map

--- lib/presentation/common_widgets/widget_home_poster.dart
+++ lib/presentation/common_widgets/widget_home_poster.dart
@@ -1,10 +1,20 @@
 import 'package:find_seat/presentation/common_widgets/barrel_common_widgets.dart';
 import 'package:find_seat/presentation/custom_ui/svg_image.dart';
+import 'package:find_seat/presentation/router.dart';
 import 'package:find_seat/utils/my_const/my_const.dart';
 import 'package:flutter/material.dart';
 
 class WidgetHomePosters extends StatelessWidget {
-  List<ItemPosterVM> items = [];
+  List<ItemPosterVM> items = [
+    ItemPosterVM('images/events/912b4ea18698ad3b3519003c1c65c97e@2x.png',
+        'Happy Halloween 2K19', 'Music show'),
+    ItemPosterVM('images/events/ef0453e3c63b754f983e6863fa5c1583@2x.png',
+        'Music DJ king monger Sertical Visa', 'Music show'),
+    ItemPosterVM('images/events/8cb99f7a9423e9a20e26dd5468668517@2x.png',
+        'Summer sounds festival', 'Comedy show'),
+    ItemPosterVM('images/events/e215ef68a07a9baa50e04b05a79970ae@2x.png',
+        'Happy Halloween 2K19', 'Music show'),
+  ];
 
   String label;
   String iconPath;
@@ -73,32 +83,44 @@ class _WidgetItemPoster extends StatelessWidget {
 
   _WidgetItemPoster(this.item);
 
+  BuildContext _context;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 93,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.asset(
-              item.photo,
-              width: 93,
-              height: 124,
-              fit: BoxFit.contain,
+    _context = context;
+    return GestureDetector(
+      onTap: () {
+        openShowDetails();
+      },
+      child: Container(
+        width: 93,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Image.asset(
+                item.photo,
+                width: 93,
+                height: 124,
+                fit: BoxFit.contain,
+              ),
             ),
-          ),
-          WidgetSpacer(height: 6),
-          Text(item.title,
-              style: FONT_CONST.REGULAR_BLACK2_12,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis),
-          WidgetSpacer(height: 2),
-          Text(item.subTitle, style: FONT_CONST.REGULAR_GRAY6_10),
-        ],
+            WidgetSpacer(height: 6),
+            Text(item.title,
+                style: FONT_CONST.REGULAR_BLACK2_12,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis),
+            WidgetSpacer(height: 2),
+            Text(item.subTitle, style: FONT_CONST.REGULAR_GRAY6_10),
+          ],
+        ),
       ),
     );
+  }
+
+  void openShowDetails() {
+    Navigator.pushNamed(_context, Router.SHOW_INFO);
   }
 }
 
