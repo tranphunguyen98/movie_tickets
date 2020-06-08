f6f0f85b4695d5365f3a0a166aca2bf7f6f0b913
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2020-06-08 22:56:49 +0700

    feat(Auth): basically define auth bloc and behavior

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:find_seat/app_config.dart';
 import 'package:find_seat/presentation/router.dart';
 import 'package:flutter/material.dart';
 
-import 'my_app.dart';
+import 'app/my_app.dart';
 
 void main() {
   MyApp.initSystemDefault();
@@ -11,8 +11,8 @@ void main() {
     AppConfig(
       appName: "FindSeat Dev",
       flavorName: "dev",
-      initialRoute: Router.LIST_ALL_CINE,
-      child: MyApp(),
+      initialRoute: Router.BOOK_SEAT_SLOT,
+      child: MyApp.runWidget(),
     ),
   );
 }
